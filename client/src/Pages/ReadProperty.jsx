bf16674d4bbabfaca47ae7af037ee0bc07da0b0c
Wajid Shaikh <wajid777shaikh@gmail.com>
2024-02-18 19:47:35 +0530

    alt images updated

--- client/src/Pages/ReadProperty.jsx
+++ client/src/Pages/ReadProperty.jsx
@@ -170,7 +170,7 @@ const ReadProperty = () => {
                         >
                           <img
                             src={`${API}/${image}`}
-                            alt=""
+                            alt="https://images.pexels.com/photos/731082/pexels-photo-731082.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                             className="object-cover hover:scale-125 transition-all ease-in-out duration-500"
                             style={{ width: "100%", height: "100%" }}
                             width={120}

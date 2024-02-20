29eae275685fdbe051f11716324d81852b6b8809
Wajid Shaikh <wajid777shaikh@gmail.com>
2024-02-21 00:07:26 +0530

    cors , payload updated

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = 8000;
 require("./db/conn");
 const cors = require("cors");
 const Router = require("./routes/route");
 
 app.use("/uploads", express.static("uploads"));
+
+// Increase payload size limit (e.g., 10MB)
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 // Allow requests from washrealestate.vercel.app
 const corsOptions = {

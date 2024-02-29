d8930c69a1475fe00cfd46e38328d32199bf8645
Wajid Shaikh <wajid777shaikh@gmail.com>
2024-02-29 14:11:58 +0530

    updated

--- client/src/Pages/Auth User/LoginUser.jsx
+++ client/src/Pages/Auth User/LoginUser.jsx
@@ -24,7 +24,7 @@ const LoginUser = ({}) => {
       setPassword("");
       setTimeout(() => {
         alert("Login successful");
-        history("/api/properties");
+        history("/");
       }, 1000);
     } catch (error) {
       setError("Invalid username or password");
